fab7e6b37c39ad4a1ed0ab34fbb2855962386b51
danang507 <danangwijayanto507@gmail.com>
2022-03-24 19:05:49 +0700

    final Program

---
+++ taxi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi_app
+{
+    public class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty);
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+            Console.WriteLine("\n");
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.Write(DriverName);
+            Console.Write(" sedang menjemput penumpang ");
+            Console.WriteLine(" ");
+
+
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.Write(DriverName);
+            Console.Write(" selesai mengantar penumpang ");
+            Console.WriteLine("\n");
+        }
+
+    }
+}
